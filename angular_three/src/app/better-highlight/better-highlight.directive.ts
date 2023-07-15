68feb0fea343f60f14aa3541765007dab8642755
rails -v <devashree.gosavi@gmail.com>
2023-07-15 10:59:28 +0530

    Initial commit

---
+++ angular_three/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  constructor(private elRef : ElementRef, private renderer :Renderer2)
+  {
+
+  }
+  ngOnInit(){
+    
+  }
+  @HostListener('mouseenter') mouseover(eventData : Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+  @HostListener('mouseleave') mouseleave(eventData : Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+  }
+}
